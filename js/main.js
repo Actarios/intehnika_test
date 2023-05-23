f02d7824386883ec0b6b4c568b4c501ff224c01c
Dimitry <di@timelabs.ru>
2023-05-23 11:48:30 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function(e) {
     const selectAllBtn = $('#select_all')
     const selectProduct = $('.product .checkbox')
     const deleteSelected = $('.delete-select')
-    const removeFromCard = $('.product__icons')
+    const removeFromCard = $('.product__icons_remove')
+    const vaforiteProduct = $('.product__icons_favorite')
+    const productAmount = $('.product__amount')
 
     selectAllBtn.on('click', () => {
         selectProduct.prop('checked', true)
@@ -17,5 +19,19 @@ jQuery(document).ready(function(e) {
         })
     })
 
+    removeFromCard.on('click', (e) => {
+        $(e.target).parents('.product')[0].remove()
+    })
+
+    vaforiteProduct.on('click', (e) => {
+        const elem = $(e.target)[0]
+        if ($(e.target).hasClass('active')) {
+            elem.src = './images/heart.svg'
+            $(e.target).removeClass('active')
+        } else {
+            elem.src = './images/heart_active.svg'
+            $(e.target).addClass('active')
+        }
+    })
    
 });
